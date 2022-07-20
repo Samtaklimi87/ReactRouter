23ffee6bc8a52d2749519274a871bad91316718c
Sam Taklimi <sam.taklimi87@gmail.com>
2022-07-20 15:13:17 +0200

    fixing the styles and fixing routers due to outdated tutorial, router version 6 works as i have deployed here

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <header>
-        <h1>Header</h1>
+      <h1>Header</h1>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Router>
-      <Route path="/" component={App} />
+        {/* <Route path="/" element={<App />} /> */}
+        <App/>
     </Router>
   </React.StrictMode>
 );
