c95fc4f1076be6e3e74e83d904ce1c6f3f0bcd81
Sam Taklimi <sam.taklimi87@gmail.com>
2022-07-20 21:37:07 +0200

    adding api posts and modify crud operations such handle delete, handle submit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Missing from "./Missing";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import {format} from 'date-fns';
+import api from './api/posts';
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -20,6 +21,25 @@ function App() {
   const navigate = useNavigate();
 
   useEffect(() => {
+    const fetchPosts = async () => {
+      try{
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch(err) {
+        // not in 200 response range
+        if(err.response) { 
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    }
+    fetchPosts();
+  },[])
+
+  useEffect(() => {
     const filteredResults = posts.filter( post => 
       ((post.body).toLowerCase()).includes(search.toLowerCase())
         || ((post.title).toLowerCase()).includes(search.toLowerCase())
@@ -27,22 +47,34 @@ function App() {
       setSearchResults(filteredResults.reverse());
   }, [posts, search])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length -1].id +1 : 1 ;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title: postTitle, datetime, body: postBody};
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    navigate('/');
+    try{
+      const response = await api.post('./posts', newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      navigate('/');
+    } catch(err){
+      console.log(`Error: ${err.message}`);
+    }
+   
   };
 
-  const handleDelete = (id) => {
-    const postsList = posts.filter((post) => post.id !== id);
-    setPosts(postsList);
-    navigate("/");
+  const handleDelete = async (id) => {
+    try{
+      await api.delete(`/posts/${id}`);
+      const postsList = posts.filter((post) => post.id !== id);
+      setPosts(postsList);
+      navigate("/");
+    } catch(err) {
+      console.log(`Error: ${err.message}`);
+    }
+    
   };
 
   return (
