bc78916f31c718c25e5813ad6700deb155597ad5
Sam Taklimi <sam.taklimi87@gmail.com>
2022-07-21 11:56:41 +0200

    adding fa icons to project by npm i, refactoring hooks by removing clean up function by passing return

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -19,9 +19,10 @@ const useWindowSize = () => {
     window.addEventListener("resize", handleResize);
 
     const cleanUp = () => {
-      console.log("runs if a useEffect dep changes");
+      console.log("runs if  a useEffect dep changes");
       window.removeEventListener("resize", handleResize);
     };
+    return cleanUp;
   }, []);
 
   return windowSize;
